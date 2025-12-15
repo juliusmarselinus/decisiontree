fc5211fbcd9dd8752a3b983a2b612bef2fffe948
juliusmarselinus <juliusmarselinus9@gmail.com>
2025-12-15 07:26:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+from flask import Flask, request, jsonify
+from prediction import predict
 
+
+app = Flask(__name__)
+
+
+@app.route("/predict", methods=["POST"])
+def predict_api():
+data = request.json
+result = predict(data)
+return jsonify(result)
+
+
+
+
+if __name__ == "__main__":
+app.run(debug=True)
