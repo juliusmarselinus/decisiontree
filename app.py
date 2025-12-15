0abaeaf4642e53156675ace6a3326846ea59df50
juliusmarselinus <juliusmarselinus9@gmail.com>
2025-12-15 07:36:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 from prediction import predict
 
+def main():
+    input_data = {
+        "Age": 35,
+        "MonthlyCharges": 70.5,
+        "Contract": 1
+    }
 
-# Contoh input (sesuaikan kolom dataset)
-input_data = {
-"Age": 35,
-"MonthlyCharges": 70.5,
-"Contract": 1
-}
+    prediction, confidence = predict(input_data)
+
+    print("Hasil Prediksi")
+    print("--------------")
+    print("Prediction:", prediction)
+    print("Confidence:", round(confidence, 2))
 
 
-prediction, confidence = predict(input_data)
-
-
-print("Hasil Prediksi")
-print("--------------")
-print("Prediction:", prediction)
-print("Confidence:", round(confidence, 2))
+if __name__ == "__main__":
+    main()
