14a780c7e25bea0cdd570530ce863ff50c3e5d02
juliusmarselinus <juliusmarselinus9@gmail.com>
2025-12-15 07:27:32 +0700

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,1 +1,32 @@
 
+import joblib
+import pandas as pd
+
+
+model = joblib.load("decision_tree_model.pkl")
+features = joblib.load("features.pkl")
+
+
+
+
+def predict(input_data: dict):
+"""
+input_data contoh:
+{
+"Age": 35,
+"MonthlyCharges": 70.5,
+"Contract": 1
+}
+"""
+df = pd.DataFrame([input_data])
+df = df[features]
+
+
+prediction = model.predict(df)[0]
+probability = model.predict_proba(df).max()
+
+
+return {
+"prediction": int(prediction),
+"confidence": float(probability)
+}
