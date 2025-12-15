9849868830f966d61b6c3131379e09b79713143a
juliusmarselinus <juliusmarselinus9@gmail.com>
2025-12-15 07:37:04 +0700

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,20 +1,15 @@
 import joblib
 import pandas as pd
 
-
 model = joblib.load("decision_tree_model.pkl")
 features = joblib.load("features.pkl")
 
 
+def predict(input_data):
+    df = pd.DataFrame([input_data])
+    df = df[features]
 
+    prediction = model.predict(df)[0]
+    confidence = model.predict_proba(df).max()
 
-def predict(input_data: dict):
-df = pd.DataFrame([input_data])
-df = df[features]
-
-
-prediction = model.predict(df)[0]
-confidence = model.predict_proba(df).max()
-
-
-return prediction, confidence
+    return prediction, confidence
